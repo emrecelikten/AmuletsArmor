84afbe6d1303b101933d86942bc8d757a7be3b24
Emre Çelikten <emrecelikten@users.noreply.github.com>
2016-10-23 15:42:47 +0300

    Add missing deserialization.h (whoops)

---
+++ include/amulets/platform/deserialization.h
@@ -0,0 +1,15 @@
+//
+// Created by Emre Çelikten on 23/10/2016.
+//
+
+#ifndef AMULETSARMOR_SERIALIZATION_H
+#define AMULETSARMOR_SERIALIZATION_H
+#include "IRESOURC.H"
+
+T_sword32
+LoadResourceFileHeader(T_file file, T_resourceFileHeader *resourceFileHeader);
+
+T_sword32
+LoadResourceEntries(T_file file, T_resourceEntry *resourceEntry, T_word16 numEntries);
+
+#endif //AMULETSARMOR_SERIALIZATION_H
